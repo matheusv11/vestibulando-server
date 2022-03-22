08edcb92bfd1ff6dd9ce6faa752951612b1c2766
matheus.silva <matheus.silva@icolabora.com>
2022-03-21 23:56:09 -0300

    Adições pendentes

---
+++ src/interfaces/lixo.ts
@@ -0,0 +1,17 @@
+// type tplotOptions = {
+//     [key: string]: boolean
+// }
+
+// interface IObjectKeys {
+//     [key: string]: string;
+//   }
+  
+// interface trainInfo {
+//     // name: keyof typeof plotOptions;
+//     x: Array<number>;
+//     y: Array<number>;
+//     type: string;
+//     mode: string;
+// }
+
+// export type ReadingTypes = 'some'|'variants'|'of'|'strings';
