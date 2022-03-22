40b886b5ed3d5c6a4a1779cd9b6753d963ab4fd0
matheus.silva <matheus.silva@icolabora.com>
2022-03-21 23:55:16 -0300

    Novas rotas

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,16 @@ import { Router } from "express"
 
 const router = Router();
 
+//CONFIGS
+import joi from "./middlewares/joi";
 // CONTROLLERS
-
 import testController from "./controllers/testController";
+import userController from "./controllers/userController";
 
+// LIMITAR ROTAS
 router.get("/test", testController.test);
+
+router.get("/user", userController.get);
+router.post("/user", joi("user"), userController.create);
 
 export default router;
