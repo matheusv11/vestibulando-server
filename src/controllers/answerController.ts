2a58c31eccb1d3c8bd6c0678a02c50f1b4d1d8ad
matheusv11 <mathsales360@gmail.com>
2022-04-15 18:34:17 -0300

    Evitar resposta repetida e apenas atualizar e deletar resposta do usuario logado

--- src/controllers/answerController.ts
+++ src/controllers/answerController.ts
@@ -16,16 +16,28 @@ export default {
     async create(req: Request, res: Response) {
         const { selected, questionId } = req.body
         const { userId } = res.locals
+
+        const answer = await  prisma.answers.findFirst({
+            where: {
+                question_id: parseInt(questionId),
+                user_id: parseInt(userId)
+            }
+        });
+
+        if(answer) return res.status(400).send({ message: "Resposta já criada"});
         
-        const correct = await prisma.questions.findUnique({
+        const question = await prisma.questions.findUnique({
             where: {
                 id: parseInt(questionId)
             }
         });
 
+        if(!question) return res.status(400).send({ message: "Questão não encontrada"});
+        
+        // VALIDAR SE O SELECTED TÁ NA FAIXA DE ALTERNATIVAS
         await prisma.answers.create({
             data: {
-                correct: correct?.answer === selected,
+                correct: question?.answer === selected,
                 selected,
                 resolution_date: new Date(), //ISO STRING
                 user_id: parseInt(userId),
@@ -35,28 +47,29 @@ export default {
 
         await prisma.$disconnect();
 
-        return res.status(201).send({ message: "Matéria criada com sucesso" });
+        return res.status(201).send({ message: "Resposta criada com sucesso" });
     },
 
     async update(req: Request, res: Response) {
-        const { selected, questionId } = req.body; //PEGAR O USER ID PELO TOKEN
+        const { selected } = req.body;
         const { id } = req.params; // USAR FORMA DE PEGA A RESPOSTA CERTA SEM PRECISAR PASSAR OUTRA ID DE QUESTAO
-        const { userId } = res.locals
+        const { userId } = res.locals;
+        // PODE ZUAR CASO MUDE A RESPOSTA AQUI, POIS VAI DIVERGIR NO CREATE
         // PEGAR RESPOSTA CERTO POR DB QUERY, PARAMS OU BODY?
         // BUSCAR PELA QUERY ABAIXO COM A RELAÇÃO DO PRISMA
 
-        const answer = await prisma.answers.findUnique({
+        const answer = await prisma.answers.findFirst({
             where: {
-                id: parseInt(id)
+                id: parseInt(id),
+                user_id: parseInt(userId)
             }
         });
-        // VALIDAR SE É O DO USUARIO TAMBÉM
+
         if(!answer) return res.status(400).send({ message: "Resposta não encontrada"});
     
-        // VALIDAR O QUESTION ID
         const correct = await prisma.questions.findUnique({
             where: {
-                id: parseInt(questionId)
+                id: answer.question_id
             }
         }); 
         
@@ -80,9 +93,10 @@ export default {
         const { id } = req.params;
         const { userId } = res.locals
 
-        const answer = await prisma.answers.findUnique({
+        const answer = await prisma.answers.findFirst({
             where: {
-                id: parseInt(id)
+                id: parseInt(id),
+                user_id: parseInt(userId)
             }
         });
 
